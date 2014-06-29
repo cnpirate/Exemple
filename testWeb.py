d9f4b748d6f090d6a670829a57364c43713b0ae6
cnpirate <zhaixing@qq.com>
2014-06-29 13:08:50 +0800

    Parse the Capital Library Website

--- testWeb.py
+++ testWeb.py
@@ -1,41 +1,19 @@
-# import urllib.request
-# from bs4 import BeautifulSoup
-# import pickle as p
+import urllib.request
+import re
+import argparse
+from bs4 import BeautifulSoup 
 
-# webURL = 'http://www.baidu.com' 
+webURL = "http://www.bplisn.net.cn/"
+u = urllib.request.urlopen(webURL)
+content = u.read()
+soup = BeautifulSoup(content)
 
-# u = urllib.request.urlopen(webURL)
-# buffer = u.read()
-# print(len(buffer))
-# print(u.info())
-
-# soup = BeautifulSoup(buffer)
-
-# f = open("test.html", "w")
-# p.dump(soup, f)
-# f.close()
-
-# import urllib.request
-# import re
-# import argparse
-# from bs4 import BeautifulSoup 
-
-# webURL = "http://www.bplisn.net.cn/"
-# u = urllib.request.urlopen(webURL)
-# content = u.read()
-# soup = BeautifulSoup(content)
-
-# itemList = soup.find_all(div=re.compile("读者卡号".encode("utf-8")))
-# for item in itemList:
-	# try:
-		# print(item.string)
-	# except AttributeError:
-		# continue
+itemList = soup.find_all('div')
+for item in itemList:
+	try:
+		print(item.find('a').string)
+	except AttributeError:
+		continue
 		
-import smtplib
-
-ser = smtplib.SMTP()
-ser.set_debuglevel(1)
-ser.connect("smtp.qq.com")
 
 
